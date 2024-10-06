286d6fcdbc56314984849f5d22ed64573c21fe10
Humaid Alam Ansari <28.humaedaalam@gmail.com>
2024-10-06 12:36:27 +0530

    second commit on 06102024

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import pickle
+
+famous_books=pickle.load(open('models/famous.pkl','rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           book_name=list(famous_books['Book-Title'].values),
+                           author=list(famous_books['Book-Author'].values),
+                           image=list(famous_books['Image-URL-M'].values),
+                           votes=list(famous_books['RatingCount'].values),
+                           rating=list(famous_books['RatingMean'].values))
 
 if __name__=='__main__':
     app.run(debug=True)
