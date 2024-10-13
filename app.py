5b5fd3101b5cbe2060e44004a61d6f9116968ee1
Humaid Alam Ansari <28.humaedaalam@gmail.com>
2024-10-13 14:30:20 +0530

    similarity based on authors nearly completed

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pickle
 
 famous_books=pickle.load(open('models/famous.pkl','rb'))
+
+books=pickle.load(open('models/books.pkl','rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html',
-                           book_name=list(famous_books['Book-Title'].values),
-                           author=list(famous_books['Book-Author'].values),
-                           image=list(famous_books['Image-URL-M'].values),
-                           votes=list(famous_books['RatingCount'].values),
-                           rating=list(famous_books['RatingMean'].values))
+                        book_name=list(famous_books['Book-Title'].values),
+                        author=list(famous_books['Book-Author'].values),
+                        image=list(famous_books['Image-URL-M'].values),
+                        votes=list(famous_books['RatingCount'].values),
+                        rating=list(famous_books['RatingMean'].values))
 
 @app.route('/similarity')
 def similarity():
     return render_template('similarity.html')
+
+@app.route('/similarAuthors',methods=['POST'])
+def similar_authors():
+    author_name=request.form.get('authorName')
+    desiredBooks = books[books['Book-Author']==author_name]
+    # return desiredBooks.to_dict(orient="records")
+    return render_template("similarity.html",data=desiredBooks.to_dict(orient="records"))
+    
+
 
 if __name__=='__main__':
     app.run(debug=True)
